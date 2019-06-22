c6a7c07340190b342c01acffa83e3792fde6d0f6
Jasmine <jeuyoquea@gmail.com>
2019-06-21 21:53:48 -0400

    Added styling to results

--- src/ZipResult.js
+++ src/ZipResult.js
@@ -3,11 +3,11 @@ import React from 'react';
 class ZipResult extends React.Component{
     render(){
         return(// the ones below are accessing info from file based on the input
-            <div>
-                <label>{this.props.City}</label><br />
-                <label>{this.props.State}</label><br />
-                <label>{this.props.EstimatedPopulation}</label><br />
-                <label>{this.props.TotalWages}</label><br />
+            <div className ="zipres">
+                <label>City: {this.props.City}</label><br />
+                <label>State: {this.props.State}</label><br />
+                <label>Population: {this.props.EstimatedPopulation}</label><br />
+                <label>Wages: {this.props.TotalWages}</label><br />
                
             </div>
         );
